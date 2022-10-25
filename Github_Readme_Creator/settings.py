9add294c043ce44b76c3acf24de5023e61519260
Shreyash Mishra <72146041+Shreyash-cyber@users.noreply.github.com>
2022-10-25 13:16:05 +0530

    Initial

--- Github_Readme_Creator/settings.py
+++ Github_Readme_Creator/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'multiselectfield',
     'personal_readme',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
